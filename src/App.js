cc9a270298c308095168d11941101d436584dba1
Lorelai <nadja.rathenow@rub.de>
2022-04-18 23:09:26 +0200

    errorpage added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import "./styles/styles.scss";
-
 import StartPage from './pages/StartPage';
 import FilmPage from './pages/FilmPage';
 import FilmDetailPage from './pages/FilmDetailPage';
 import FavPage from './pages/FavPage';
+import ErrorPage from './pages/ErrorPage';
 
 import './styles/styles.css'
 
 function App() {
   return (
-      <BrowserRouter>
-			<Routes>
-				<Route path="/" element={<StartPage />} />
-				<Route path="films" element={<FilmPage />} />
-				<Route path="films/:id" element={<FilmDetailPage />} />
-        <Route path="favs" element={<FavPage />} />
-			</Routes>
-		</BrowserRouter>
+		<Routes>
+			<Route path="/" element={<StartPage />} />
+			<Route path="films" element={<FilmPage />} />
+			<Route path="films/:id" element={<FilmDetailPage />} />
+        	<Route path="favs" element={<FavPage />} />
+			<Route path="*" element={ <ErrorPage /> } />
+		</Routes>
     );
 }
 
